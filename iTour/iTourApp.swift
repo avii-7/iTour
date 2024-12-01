4c8c298e442090d468a66f892830bc8111a516e5
Arun Kumar <kumar.arun.ynr@gmail.com>
2024-12-01 15:43:48 +0530

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Arun on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
